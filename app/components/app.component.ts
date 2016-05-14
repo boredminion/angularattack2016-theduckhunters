d665956878421862b57e2301e76dcb467f0a33ca
Niraj Rajbhandari <nirajrajbhandari@lftechnology.com>
2016-05-14 07:49:11 +0545

    creates skeleton for the app

---
+++ app/components/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: "duck-hunter",
+    template: `
+        <h1>Duck Hunters</h1>
+        <p>Play the game.!!!!</p>
+    `
+})
+export class AppComponent {
+    constructor() {
+
+    }
+}
