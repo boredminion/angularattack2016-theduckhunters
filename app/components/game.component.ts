b60bdafb78cca671d5b4df35351126e9534dcd01
joshianuj <joshi.anujo7@gmail.com>
2016-05-14 12:55:13 +0545

    DH-3 Add Game loop

--- app/components/game.component.ts
+++ app/components/game.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component,ChangeDetectionStrategy} from '@angular/core';
 
 //socket
 import {SocketService} from '../services/socket.service';
@@ -7,9 +7,15 @@ import {SocketEvents} from '../services/socket_events.enum';
 @Component({
     templateUrl: 'app/assets/templates/game.html',
     styleUrls: ['app/assets/css/style.css'],
+    changeDetection:ChangeDetectionStrategy.OnPush
 })
 
 export class GameComponent {
+
+    private userInfo;
+    private payloadGrid;
+    private players;
+
     constructor(private socketService:SocketService) {
     }
 
@@ -17,8 +23,12 @@ export class GameComponent {
      * Allows user to join the game
      */
     public joinGame() {
-        this.socketService.getSocket().emit(SocketEvents[SocketEvents.joinGame], {}, ()=>{
+        this.socketService.getSocket().emit(SocketEvents[SocketEvents.joinGame], {}, (response)=> {
+            this.payloadGrid = response.payloadGrid;
+            this.userInfo = response.userInfo;
+            this.players = response.players;
             console.log("user successfully joined");
         });
     }
+    
 }
